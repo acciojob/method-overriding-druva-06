a2d40f37bb66ddb9ec9ef0dc7554f665ad668f74
Druvasena Reddy Naini <nainidruvasenareddy3737@gmail.com>
2023-03-19 22:16:57 +0530

    Overriding Modified

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -12,7 +12,7 @@ class B extends A{
 }
 public class Main {
     public static void main(String args[]){
-        B obj = new B();
+        A obj = new B();
         System.out.println(obj.meth());
     }
 }
